bc94bb6bb92f6224fdee271de4d06ee41b3fa219
khanidrees <khanidrees1234@gmail.com>
2024-05-10 16:51:30 +0530

    read with zlib

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,7 @@ import java.nio.file.Path;
 public class Main {
   public static void main(String[] args){
     // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
+
 
     // Uncomment this block to pass the first stage
 
